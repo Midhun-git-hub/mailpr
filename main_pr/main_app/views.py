84bfaf8af70d4614dbd51da542e5eaf8104dc662
Midhun-git-hub <m20787549@gmail.com>
2026-02-20 14:34:47 +0530

    Configured Celery SSL for Upstash Redis

--- main_pr/main_app/views.py
+++ main_pr/main_app/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view , permission_classes
 from rest_framework.permissions import IsAuthenticated,AllowAny
 from rest_framework.response import Response
 from rest_framework import status
-from main_pr.main_app.tasks import send_test_email_task
+from .tasks import send_test_email_task
 from .serializers import *
 from .models import *
 from rest_framework.views import APIView
