1bbdfc520a9ba9fe666605838a5a2d7afed4ced8
Midhun-git-hub <m20787549@gmail.com>
2026-02-20 10:59:06 +0530

    Configured Celery SSL for Upstash Redis

--- main_pr/main_pr/celery.py
+++ main_pr/main_pr/celery.py
@@ -1,4 +1,5 @@
 import os
+import ssl
 from celery import Celery
 from celery.schedules import crontab
 from dotenv import load_dotenv
@@ -11,16 +12,27 @@ app = Celery("main_pr")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
+# Redis (Upstash)
 app.conf.broker_url = os.getenv("REDIS_URL")
 app.conf.result_backend = os.getenv("REDIS_URL")
 
+# Required for rediss:// (Upstash SSL)
+app.conf.broker_use_ssl = {
+    "ssl_cert_reqs": ssl.CERT_NONE
+}
+
+app.conf.redis_backend_use_ssl = {
+    "ssl_cert_reqs": ssl.CERT_NONE
+}
+
+# Scheduled tasks
 app.conf.beat_schedule = {
     "send-daily-emails": {
         "task": "main_app.tasks.send_daily_emails",
-        "schedule": crontab(hour=8, minute=0),  # Every day at 8:00 AM
+        "schedule": crontab(hour=8, minute=0),
     },
     "send-weekly-emails": {
         "task": "main_app.tasks.send_weekly_emails",
-        "schedule": crontab(hour=9, minute=0, day_of_week=1),  # Every Monday at 9:00 AM
+        "schedule": crontab(hour=9, minute=0, day_of_week=1),
     }
 }
