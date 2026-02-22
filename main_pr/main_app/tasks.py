4e26d1d9ea407362702d6de6f2f5e1316282022b
Midhun-git-hub <m20787549@gmail.com>
2026-02-22 11:56:40 +0530

    Configured smpt to resend mail

--- main_pr/main_app/tasks.py
+++ main_pr/main_app/tasks.py
@@ -19,7 +19,7 @@ def send_resend_email(to_email, subject, message):
             "Content-Type": "application/json",
         },
         json={
-            "from": "onboarding@resend.dev",
+            "from": "onboarding@mailpr.onrender.com",
             "to": [to_email],
             "subject": subject,
             "text": message,
